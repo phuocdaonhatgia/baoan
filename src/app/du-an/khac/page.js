8f29d2ecf553f3c09d5998f163493226375b3c2c
phuocdaonhatgia <nhatgiaphuoc.dao@gmail.com>
2026-07-28 11:34:10 +0700

    27th push

--- src/app/du-an/khac/page.js
+++ src/app/du-an/khac/page.js
@@ -6,9 +6,9 @@ export const metadata = {
 }
 
 const projects = [
-  { name: 'Karaoke Thái Bình', location: 'Đồng Hới', systems: ['Sprinkler tự động', 'Báo cháy tự động','Chữa cháy vách tường','Chữa cháy Sol Khí cho các phòng hát','Hệ thống Cửa thoát hiểm, Chống Sét','Đèn chiếu sáng sự cố và thoát hiểm'], img: '/images/projects/thaibinh.jpg' },
-  { name: 'Trung tâm kho vận Yên Nghĩa', location: 'Hà Nội', systems: ['Hệ thống báo cháy tự động'], img: '/images/projects/kho van.png' },
-  { name: '	Phòng khám 2 – Viện Tim TP.HCM', location: 'Hồ Chí Minh', systems: ['Sprinkler tự động','Hệ thộng báo cháy','Chữa cháy vách tường'], img: '/images/projects/vientim.jpg' },
+  { slug:'karaoke-thai-binh', name: 'Karaoke Thái Bình', location: 'Đồng Hới', systems: ['Sprinkler tự động', 'Báo cháy tự động','Chữa cháy vách tường','Chữa cháy Sol Khí cho các phòng hát','Hệ thống Cửa thoát hiểm, Chống Sét','Đèn chiếu sáng sự cố và thoát hiểm'], img: '/images/projects/thaibinh.jpg' },
+  {  slug: 'trung-tam-kho-van-yen-nghia', name: 'Trung tâm kho vận Yên Nghĩa', location: 'Hà Nội', systems: ['Hệ thống báo cháy tự động'], img: '/images/projects/kho van.png' },
+  { slug: 'phong-kham-2-vien-tim-tphcm', name: 'Phòng khám 2 – Viện Tim TP.HCM', location: 'Hồ Chí Minh', systems: ['Sprinkler tự động','Hệ thộng báo cháy','Chữa cháy vách tường'], img: '/images/projects/vientim.jpg' },
       {slug: 'truong-phap-quoc-te-marguerite-duras', name: 'Trường pháp Quốc tế Marguerite Duras', location: 'Hồ Chí Minh.', systems: ['Chữa cháy vách tường','Báo cháy tự động'], img: '/images/projects/duras2.jpg' },
   {slug:'nha-huu-duong-rosa-lima', name: 'Nhà hưu dưỡng của Hội dòng Đa Minh Rosa Lima ', location: 'Hồ Chí Minh', systems: ['Thiết kế kiến trúc và PCCC'], img: '/images/projects/rosa-lima.jpg' },
 ]
